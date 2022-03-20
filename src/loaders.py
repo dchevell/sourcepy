bc3762dbdb03f754912d9fd7574236b5ec47955e
Dave Chevell <chevell@gmail.com>
2022-03-20 14:14:17 +1100

    Remove recursion in sourcing, better filtering of locals vs imports

--- src/loaders.py
+++ src/loaders.py
@@ -6,7 +6,7 @@ import sys
 from collections.abc import Callable
 from pathlib import Path
 from types import ModuleType
-from typing import Any, Iterator, List, Optional, get_origin
+from typing import Any, Iterator, List, Optional
 
 from casters import isprimitive, iscollection
 
@@ -26,23 +26,23 @@ def load_path(module_path: Path) -> ModuleType:
     return module
 
 
-def get_definitions(obj: Any, parents: Optional[List] = None) -> Iterator[dict]:
-    if parents is None:
-        parents = []
-    members = dict(inspect.getmembers(obj))
+def get_definitions(module: ModuleType) -> Iterator[dict]:
+    members = dict(inspect.getmembers(module))
     if '__all__' in members:
         members = {k:v for k,v in members.items() if k in members['__all__']}
     else:
         members = {k:v for k,v in members.items() if not k.startswith('_')}
+
     for name, value in members.items():
-        if is_unsourceable(value):
+        if isinstance(value, type):
             continue
-        if isprimitive(value) or iscollection(value):
-            yield {'type': 'variable', 'name': name, 'value': value, 'parents': parents}
+        if inspect.getmodule(value) not in (module, None):
+            continue
+        if (isprimitive(value) or iscollection(value)):
+            yield {'type': 'variable', 'name': name, 'value': value}
         elif callable(value):
-            yield {'type': 'function', 'name': name, 'value': value, 'parents': parents}
-        else:
-            yield from get_definitions(value, parents + [name])
+            yield {'type': 'function', 'name': name, 'value': value}
+
 
 
 def get_callable(parent: ModuleType, method_str: str) -> Callable:
@@ -53,9 +53,3 @@ def get_callable(parent: ModuleType, method_str: str) -> Callable:
     if not callable(current):
         raise ValueError(f'{method_str} does not point to a valid callable')
     return current
-
-def is_unsourceable(obj: Any) -> bool:
-    return any([
-        isinstance(obj, (type, ModuleType)),
-        isinstance(get_origin(obj), type),
-    ])
